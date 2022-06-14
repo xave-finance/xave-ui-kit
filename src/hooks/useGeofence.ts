a17ce05781dd4a5bfe0b4eb37954144cf514d024
Tracy <teresa.arciaga@gmail.com>
2022-06-14 15:07:02 +0800

    Add url parameter

--- src/hooks/useGeofence.ts
+++ src/hooks/useGeofence.ts
@@ -1,50 +1,39 @@
 // Hook that automatically geofences an app
-import useAxios from 'axios-hooks'
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 export enum GeofenceCountry {
-  PHILIPPINES = 'PH',
-  SINGAPORE = 'SG',
-  US = 'US',
+  PHILIPPINES = "PH",
+  SINGAPORE = "SG",
+  US = "US",
+ 
 }
 
 // and then redirects to a URL if not applicable
-export const useGeofence = (country: GeofenceCountry) => {
+export const useGeofence = (country: GeofenceCountry, url: string) => {
   // If rejected, redirect to rejectedUrl
-  const [rejected, setRejected] = useState(true)
-  // const rejected = true
+  const [rejected, setRejected] = useState(false)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-
-    const [{ data, loading }] = useAxios(
-      'https://ipapi.co/country'
-    )
-    if (!loading) setLoading(false)
-
-    if (data === country) {
-      setRejected(true)
-    } else {
-      setRejected(false)
-    }
-    // setLoading(loading)
-    //   axios
-    //     .get("https://ipapi.co/country")
-    //     .then(({ data }) => {
-    //       if (data === country) {
-    //         setRejected(true)
-    //       } else {
-    //         setRejected(false)
-    //       }
-    //     })
-    //     .catch((e) => {
-    //       // Geofence failed. Should we default to rejected or ok?
-    //       console.log(e)
-    //       setRejected(false)
-    //     })
-    //     .finally(() => {
-    //       setLoading(false)
-    //     })
+    setLoading(true)
+    axios
+      .get(url)
+      .then(({ data }) => {
+        if (data === country) {
+          setRejected(true)
+        } else {
+          setRejected(false)
+        }
+      })
+      .catch((e) => {
+        // Geofence failed. Should we default to rejected or ok?
+        console.log(e)
+        setRejected(false)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [country])
 
   return {
